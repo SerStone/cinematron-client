9c696aa7fdb4a37b161a27b0ff28503bc250d630
Bohdan <bohdan.duda.it@gmail.com>
2023-10-21 23:29:08 +0300

    feat: comment section and all related to it components created; ctreated layout for comments; added fetching, creating and deleting login to comments

---
+++ src/services/comments.service.ts
@@ -0,0 +1,38 @@
+import {axiosServiceUser} from "./axios.service";
+import {urls} from "../constants";
+import {IRes} from "../types/axiosRes.type";
+import {IComment} from "../interfaces/comment.interface";
+
+export class CommentsService {
+    static async getByMovieId(id: string): Promise<IComment[]> {
+        try {
+            const res = await axiosServiceUser.get(urls.comments.commentsByMovieId(id))
+            return res.data;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    static async createOne(userId: string, userName: string, movieId: string, text: string): Promise<IComment> {
+        const body = {
+            userId,
+            userName,
+            movieId,
+            text,
+        }
+
+        try {
+            return await axiosServiceUser.post(urls.comments.add, body)
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    static async delete(id: string, userId: string): Promise<IComment> {
+        try {
+            return await axiosServiceUser.delete(urls.comments.delete(id), {data: {userId}})
+        } catch (e) {
+            throw e;
+        }
+    }
+}
